39a836265bcf6827b6ee7a5a492d7f0e55667da6
KILLinefficiency <shreyas.sable2166@gmail.com>
2020-12-11 00:29:45 +0530

    added support for default and verbose in settings

--- forge.go
+++ forge.go
@@ -5,12 +5,17 @@ import (
   "fmt"
   "os/exec"
   "strings"
+  "strconv"
   "io/ioutil"
   "encoding/json"
 )
 
 var forgeMe map[string]interface{}
 var heads map[string]interface{}
+var settings map[string]interface{}
+
+var defaultHead string
+var verbose bool = true
 
 func keyExists(reqKey string, data map[string]interface{}) bool {
   for key, _ := range data {
@@ -25,10 +30,18 @@ func strExec(shellCommand string) {
   commandArgs := strings.Split(shellCommand, " ")
   commandExec := exec.Command(commandArgs[0], commandArgs[1:]...)
   _, err := commandExec.Output()
-  if err != nil {
+  if err != nil && verbose {
     fmt.Printf("%s", commandExec.Stderr)
   }
-  fmt.Printf("%v", commandExec.Stdout)
+  if verbose {
+    fmt.Printf("%v", commandExec.Stdout)
+  }
+}
+
+func sliceExec(sliceShellCommands []interface{}) {
+  for _, scriptLine := range sliceShellCommands {
+    strExec(scriptLine.(string))
+  }
 }
 
 func main() {
@@ -40,15 +53,28 @@ func main() {
 
   json.Unmarshal(jsonStream, &forgeMe)
 
-  heads = forgeMe["heads"].(map[string]interface{})
+  if keyExists("!settings", forgeMe) {
+    settings = forgeMe["!settings"].(map[string]interface{})
+
+    if keyExists("default", settings) {
+      defaultHead = settings["default"].(string)
+    }
+    if keyExists("verbose", settings) {
+      verbose, _ = strconv.ParseBool(settings["verbose"].(string))
+    }
+  }
+
+  if keyExists("!heads", forgeMe) {
+    heads = forgeMe["!heads"].(map[string]interface{})
+    if len(os.Args) == 1 {
+      sliceExec(heads[defaultHead].([]interface{}))
+    }
+  }
 
   argHeads := os.Args[1:]
   for _, head := range argHeads {
     if keyExists(head, heads) {
-      headCommands := heads[head].([]interface{})
-      for _, execCommands := range headCommands {
-        strExec(execCommands.(string))
-      }
+      sliceExec(heads[head].([]interface{}))
     }
   }
 
